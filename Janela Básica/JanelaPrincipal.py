14d26500f888d9c893988ef97170f532ed387a3c
Andriele <andriele.p.martins@aluno.senai.br>
2026-07-25 01:46:26 -0300

    Atualizações
    Dia 25/07
    01:45 💯

--- Janela Básica/JanelaPrincipal.py
+++ Janela Básica/JanelaPrincipal.py
@@ -94,6 +94,14 @@ button.pack(pady=5)
 
 # ===================================================================================
 
+#puxando a imagem do Batman
+Batman = tk.PhotoImage(file="Janela Básica/img/Batman.png")
+
+#em seguida, criando um label para exibir a imagem
+label = tk.Label(root, image=Batman)
+label.pack(expand=True)
+
+# ===================================================================================
 
 #inicia o loop principal da janela
 root.mainloop()

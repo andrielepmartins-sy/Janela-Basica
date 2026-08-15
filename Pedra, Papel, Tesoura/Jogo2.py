7f8ea118d0a7c0e10aed5120c4490b2a95211dc1
andrielepmartins-sy <andriele.p.martins@aluno.senai.br>
2026-08-14 22:20:30 -0300

    14/08
    Jogo Pedra...

--- Pedra, Papel, Tesoura/Jogo2.py
+++ Pedra, Papel, Tesoura/Jogo2.py
@@ -1,78 +1,78 @@
 from tkinter import *
 from tkinter import ttk
-from PIL import Imagem, ImageTK
+from PIL import Image, ImageTk
+import random
+
 # CORES DA INTERFACE
 
-cor0 = "white"    # Cor branca
-cor1 = "black"    # Cor preta
-cor2 = "orange"   # Cor laranja
-cor3 = "yellow"   # Cor amarela
-cor4 = "green"    # Cor verde
-cor5 = "red"      # Cor vermelha
-fundo = "gray"    # Cor do fundo da janela
+cor0 = "white"
+cor1 = "black"
+cor2 = "orange"
+cor3 = "yellow"
+cor4 = "green"
+cor5 = "red"
+fundo = "gray"
 
 
 # CRIAÇÃO DA JANELA
 
-janela = Tk()  
+janela = Tk()
 
-janela.title("Pedra, Papel, Tesoura") 
+janela.title("Pedra, Papel, Tesoura")
 
-janela.geometry("260x250")
+janela.geometry("260x330")
 
 janela.configure(bg=fundo)
 
-#==================================================== PLACAR ========================================================================   
+
+# ==================================================== PLACAR ====================================================
+
 # FRAME DE CIMA
 
-# Cria um Frame (área) na parte superior da janela
 frame_cima = Frame(
     janela,
-    width=260,       # Largura do Frame
-    height=100,      # Altura do Frame
-    bg=cor1,         # Fundo preto
-    relief="raised"  # Efeito de relevo
+    width=260,
+    height=100,
+    bg=cor1,
+    relief="raised"
 )
 
-# Posiciona o Frame na linha 0 e coluna 0
 frame_cima.grid(
     row=0,
     column=0,
     sticky=NW
 )
 
+
 # FRAME DE BAIXO
 
-# Cria o Frame que ficará na parte inferior
 frame_baixo = Frame(
     janela,
     width=260,
     height=300,
-    bg=cor0,          # Fundo branco
-    relief="flat"     # Sem relevo
+    bg=cor0,
+    relief="flat"
 )
 
-# Posiciona o Frame abaixo do frame_cima
 frame_baixo.grid(
     row=1,
     column=0,
     sticky=NW
 )
 
-# NOME DO JOGADOR
 
-# Cria um texto para identificar o jogador
+# ==================================================== JOGADOR ====================================================
+
 app_pessoa = Label(
     frame_cima,
-    text="jogador",          # Texto mostrado
+    text="jogador",
     height=1,
-    anchor="center",         # Centraliza o texto
-    bg=cor1,                 # Fundo preto
-    fg=cor0,                 # Texto branco
-    font=("Ivy 10 bold")     # Fonte e tamanho
+    anchor="center",
+    bg=cor1,
+    fg=cor0,
+    font=("Ivy 10 bold")
 )
 
-# Posiciona o nome do jogador
 app_pessoa.place(
     x=10,
     y=70
@@ -81,19 +81,16 @@ app_pessoa.place(
 
 # LINHA DO JOGADOR
 
-# Cria uma área verde que poderá ser usada
-# como parte da interface do jogador
 app_pessoa_linha = Label(
     frame_cima,
     text="",
     height=10,
     anchor="center",
-    bg=cor4,                 # Fundo verde
+    bg=cor1,
     fg=cor0,
     font=("Ivy 10 bold")
 )
 
-# Posiciona a linha/área verde
 app_pessoa_linha.place(
     x=0,
     y=0
@@ -102,27 +99,24 @@ app_pessoa_linha.place(
 
 # PONTOS DO JOGADOR
 
-# Mostra a pontuação atual do jogador
 app_pessoa_pontos = Label(
     frame_cima,
-    text="0",                # Pontuação inicial
+    text="0",
     height=1,
     anchor="center",
     bg=cor1,
     fg=cor0,
-    font=("Ivy 30 bold")     # Fonte grande
+    font=("Ivy 30 bold")
 )
 
-# Posiciona a pontuação
 app_pessoa_pontos.place(
     x=50,
     y=20
 )
 
-# SEPARADOR DA PONTUAÇÃO
 
-# Cria o ":" que ficará entre os pontos
-# Exemplo: 0 : 0
+# ==================================================== VS ====================================================
+
 app_vs = Label(
     frame_cima,
     text=":",
@@ -133,94 +127,225 @@ app_vs = Label(
     font=("Ivy 30 bold")
 )
 
-# Posiciona o separador
 app_vs.place(
     x=125,
     y=20
 )
 
 
+# ==================================================== PC ====================================================
+
 app_PC = Label(
     frame_cima,
-    text="PC",          # Texto mostrado
+    text="PC",
     height=9,
-    anchor="center",         # Centraliza o texto
-    bg=cor1,                 # Fundo preto
-    fg=cor0,                 # Texto branco
-    font=("Ivy 10 bold")     # Fonte e tamanho
+    anchor="center",
+    bg=cor1,
+    fg=cor0,
+    font=("Ivy 10 bold")
 )
+
 app_PC.place(
     x=210,
     y=6
 )
+
+
+# LINHA DO PC
 
 app_PC_linha = Label(
     frame_cima,
     text="",
     height=10,
     anchor="center",
-    bg=cor4,                 # Fundo verde
+    bg=cor1,
     fg=cor0,
     font=("Ivy 10 bold")
 )
 
-# Posiciona a linha/área verde
 app_PC_linha.place(
     x=255,
     y=0
 )
 
+
+# PONTOS DO PC
+
 app_PC_pontos = Label(
     frame_cima,
-    text="0",                # Pontuação inicial
+    text="0",
     height=1,
     anchor="center",
     bg=cor1,
     fg=cor0,
-    font=("Ivy 30 bold")     # Fonte grande
+    font=("Ivy 30 bold")
 )
 
-# Posiciona a pontuação
 app_PC_pontos.place(
     x=185,
     y=20
 )
+
+
+# ==================================================== EMPATE ====================================================
 
 app_empate = Label(
     frame_cima,
     text="",
     height=10,
     anchor="center",
-    bg=cor3,                
+    bg=cor1,
     fg=cor3,
     font=("Ivy 10 bold")
 )
 
-# Posiciona a linha/área Amarela
 app_empate.place(
     y=95,
     width=270
 )
 
-#================================================ IMAGENS ==================================================
+# ============================ INICIAR JOGO ======================================================================
+
+rodadas = 5
+
+def jogar(escolha):
+    global rodadas
+
+    print(escolha)
+
+    opçoes = ["pedra", "papel", "tesoura"]
+
+    if rodadas > 0:
+
+        escolha_PC = random.choice(opçoes)
+        escolha_pessoa = escolha
+
+        print(escolha_pessoa, escolha_PC)
+
+        rodadas -= 1
+
+    else:
+        terminar_jogo()
+
+
+def terminar_jogo():
+    print("Fim de jogo!")
+
+
+def iniciar_jogo():
+
+    # Faz os botões aparecerem
+    btn_pedra.place(
+        x=15,
+        y=30
+    )
+
+    btn_papel.place(
+        x=100,
+        y=30
+    )
+
+    btn_tesoura.place(
+        x=190,
+        y=30
+    )
+
+    # Esconde o botão Jogar
+    btn_iniciar.place_forget()
+
+# ============================ IMAGENS ============================================
+
+# PEDRA
 
 icone_pedra = Image.open("./img/pedra.png")
-icone_pedra = icone_pedra.resize((50, 50), Image.Resampling.LANCZOS)
-icone_pedra = ImageTK.PhotoImage(icone_pedra)
 
-btn_pedra = Button (frame_baixo,
-                    width=50,
-                    height=50,
-                    image=icone_pedra,
-                    bg=cor0, fg=cor0,
-                    compound="center",
-                    relief="flat")
-btn_pedra.place(
-    x=15,
-    y=60
+icone_pedra = icone_pedra.resize(
+    (50, 50),
+    Image.Resampling.LANCZOS
+)
+
+icone_pedra = ImageTk.PhotoImage(icone_pedra)
+
+
+btn_pedra = Button(
+    frame_baixo,
+    width=50,
+    height=50,
+    image=icone_pedra,
+    bg=cor0,
+    fg=cor0,
+    compound="center",
+    relief="flat",
+    command=lambda: jogar("pedra")
+
 )
 
 
+# ================================== PAPEL =========================================
+
+icone_papel = Image.open("./img/papel.png")
+
+icone_papel = icone_papel.resize(
+    (50, 50),
+    Image.Resampling.LANCZOS
+)
+
+icone_papel = ImageTk.PhotoImage(icone_papel)
+
+
+btn_papel = Button(
+    frame_baixo,
+    width=50,
+    height=50,
+    image=icone_papel,
+    bg=cor0,
+    fg=cor0,
+    compound="center",
+    relief="flat",
+    command=lambda: jogar("papel")
+)
+
+# ================================== TESOURA ==============================================
+
+icone_tesoura = Image.open("./img/tesoura.png")
+
+icone_tesoura = icone_tesoura.resize(
+    (50, 50),
+    Image.Resampling.LANCZOS
+)
+
+icone_tesoura = ImageTk.PhotoImage(icone_tesoura)
+
+
+btn_tesoura = Button(
+    frame_baixo,
+    width=50,
+    height=50,
+    image=icone_tesoura,
+    bg=cor0,
+    fg=cor0,
+    compound="center",
+    relief="flat",
+    command=lambda: jogar("tesoura")
+)
+
+
+# ============================ BOTÃO JOGAR ============================
+
+btn_iniciar = Button(
+    frame_baixo,
+    text="Jogar",
+    bg="black",
+    fg="white",
+    width=30,
+    height=2,
+    command=iniciar_jogo
+)
+
+btn_iniciar.place(
+    x=20,
+    y=150
+)
 
 
 janela.mainloop()

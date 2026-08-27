7bc41a86f1a1949bbb4e98ef1a59791ec346616d
andrielepmartins-sy <andriele.p.martins@aluno.senai.br>
2026-08-26 22:25:18 -0300

    desenhando
    desenhando 26/08

---
+++ canvas/DesenhandoPolígonos.py
@@ -0,0 +1,18 @@
+from tkinter import Tk, Canvas
+
+janela = Tk()
+janela.geometry("500x400")
+canvas= Canvas(janela, width=400, height=300, bg="yellow")
+
+
+canvas.create_polygon(
+   60, 50, 
+   10, 100, 
+   35, 150,
+   85, 150, 
+   110, 100,
+   fill='green'
+)
+
+canvas.pack()
+janela.mainloop()
